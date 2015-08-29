c5bb54104a4221aee26d002edbe3ec28b492c144
Bez Hermoso <bezalelhermoso@gmail.com>
2015-08-29 11:39:37 -0700

    applify command

--- aliases/custom.aliases.bash
+++ aliases/custom.aliases.bash
@@ -2,3 +2,18 @@ function cpkey() {
   cat $HOME/.ssh/id_rsa.pub | pbcopy
   echo 'Public key copied to clipboard'
 }
+
+function appify() {
+   APPNAME=${2:-$(basename "$1" ".sh")}
+   DIR="$APPNAME.app/Contents/MacOS"
+
+   if [ -a "$APPNAME.app" ]; then
+     echo "$PWD/$APPNAME.app already exists :("
+       exit 1
+   fi
+
+   mkdir -p "$DIR"
+   cp "$1" "$DIR/$APPNAME"
+   chmod +x "$DIR/$APPNAME"
+   echo "$PWD/$APPNAME.app"
+}
